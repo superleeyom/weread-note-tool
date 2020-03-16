3638c1c8b450b313fcea83b8685ab8fb3cc6e8ea
superleeyom <635709492@qq.com>
2020-03-16 16:15:48 +0800

    修复一些提示

---
+++ src/main/java/com/leeyom/weread/exception/SysExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.leeyom.weread.exception;
+
+
+import com.leeyom.weread.domain.Consts;
+import com.leeyom.weread.domain.ResultDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 全局异常处理
+ */
+@ControllerAdvice
+@RestController
+@Slf4j
+public class SysExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResultDto runOperateExp(RuntimeException ex) {
+        log.error("异常日志：", ex);
+        return new ResultDto(Consts.ErrCode.ERROR, "转换失败，请确认笔记格式是否正确！");
+    }
+
+    @ExceptionHandler(BizException.class)
+    public ResultDto handleBizException(BizException ex) {
+        log.error("异常日志：", ex);
+        int code = Consts.ErrCode.ERROR;
+        String message = ex.getMessage();
+        return new ResultDto(code, StringUtils.isBlank(message) ? "转换失败，请确认笔记格式是否正确！" : message);
+    }
+
+}
